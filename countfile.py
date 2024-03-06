8cb1b3a9db1342b1540e704afa8ad23cea33fec1
rolansy <ronalshoeygeorge@gmail.com>
2024-03-06 14:38:31 +0530

    Add countfile.py for counting lines, words, and characters

---
+++ countfile.py
@@ -0,0 +1,21 @@
+f=open("file1.txt","r")
+d={'U':0,'L':0,'SP':0}
+l=f.readlines()
+lc=len(l)
+wc=0
+for i in l:
+    a=i.split()
+    wc+=len(a)
+    for x in a:
+        for j in x:
+            if j.isupper():
+                d['U']+=1
+            elif j.islower():
+                d['L']+=1
+            elif not j.isalnum():
+                d['SP']+=1
+print("Line Count:",lc)
+print("Word Count:",wc)
+print("Upper Case Count:",d['U'])
+print("Lower Case Count:",d['L'])
+print("Special Character Count:",d['SP'])
