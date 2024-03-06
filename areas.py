1abfe4aa84b019eacfb18a29ec5853768e0025d3
rolansy <ronalshoeygeorge@gmail.com>
2024-03-06 13:21:29 +0530

    Add area calculation for triangle and rectangle

---
+++ areas.py
@@ -0,0 +1,9 @@
+print("Area of Triangle : ",((int(input("Enter breadth of triangle : ")))*(int(input("Enter height of triangle : "))))/2,"\n Area of Rectangle : ",(int(input("Enter length of rectangle : ")))*(int(input("Enter width of triangle : "))))
+
+'''
+b=int(input("Enter breadth of triangle : "))
+h=int(input("Enter length of triangle : "))
+l,w=map(int,input("Enter length and breadth of rectangle : ").split())
+print("Area of Triangle : ",(b*h)/2)
+print("Area of Rectangle : ",l*w)
+'''
