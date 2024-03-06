8810d93a726a3ac60d2781b81ac750d48607adcd
rolansy <ronalshoeygeorge@gmail.com>
2024-03-06 13:23:14 +0530

    Add std.py file for calculating total and average marks

---
+++ std.py
@@ -0,0 +1,8 @@
+s=input("Enter Student name: ")
+a=int(input("Enter Mark 1 : "))
+b=int(input("Enter Mark 2 : "))
+c=int(input("Enter Mark 3 : "))
+sum=a+b+c
+av=sum/3
+print(s,"'s total marks is : ",sum)
+print(s,"'s average marks is : ",av)
