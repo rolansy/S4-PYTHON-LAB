6b9d318672fac200bb0dec5686b28c4e17bc4b43
rolansy <ronalshoeygeorge@gmail.com>
2024-03-06 14:24:39 +0530

    Refactor book stock management menu

--- booksdict.py
+++ booksdict.py
@@ -1,43 +1,62 @@
-d={}
+d = {}
+
 while True:
-    print("1. Update Stock\n2. Add Stock\n3. Delete Stock\n4. Search Stock\n5. Display Stocks\n6. Exit")
-    ch=int(input("Enter your choice: "))
-    if ch==1:
-        print("Update Stock")
-        bn=input("Enter Book Name : ")
+    print("1. Update Stock")
+    print("2. Add Stock")
+    print("3. Delete Stock")
+    print("4. Search Stock")
+    print("5. Display Stocks")
+    print("6. Exit")
+
+    ch = int(input("Enter your choice: "))
+
+    if ch == 1:
+        print("\nUpdate Stock")
+        bn = input("Enter Book Name: ")
+
         if bn in list(d.keys()):
-            print(bn,"Exists")
+            print(bn, "Exists")
         else:
-            print(bn,"Doesnt Exist yet")
-            d[bn]=int(input("Enter Stock Quantity : "))
-    elif ch==2:
-        print("Add Stock")
-        bn=input("Enter Book Name : ")
+            print(bn, "Doesn't Exist yet")
+            d[bn] = int(input("Enter Stock Quantity: "))
+
+    elif ch == 2:
+        print("\nAdd Stock")
+        bn = input("Enter Book Name: ")
+
         if bn in list(d.keys()):
-            print(bn,"Exists")
-            addn=int(input("Enter Nuber of stocks yo be added : "))
-            d[bn]+=addn 
+            print(bn, "Exists")
+            addn = int(input("Enter Number of stocks to be added: "))
+            d[bn] += addn
         else:
-            print(bn,"Doesnt Exist ")
-    elif ch==3:
-        print("Delete Stock")
-        bn=input("Enter Book Name to be deleted : ")
+            print(bn, "Doesn't Exist")
+
+    elif ch == 3:
+        print("\nDelete Stock")
+        bn = input("Enter Book Name to be deleted: ")
+
         if bn in list(d.keys()):
             del d[bn]
-            print(bn,"Deleted") 
+            print(bn, "Deleted")
         else:
-            print(bn,"Doesnt Exist ")
-    elif ch==4:
-        print("Search Stock")
-        bn=input("Enter Book Name to be Searched : ")
+            print(bn, "Doesn't Exist")
+
+    elif ch == 4:
+        print("\nSearch Stock")
+        bn = input("Enter Book Name to be Searched: ")
+
         if bn in list(d.keys()):
-            print("BookName : ",bn,"\nStocks : ",d[bn]) 
+            print("Book Name:", bn)
+            print("Stocks:", d[bn])
         else:
-            print(bn,"Doesnt Exist ")
-    elif ch==5:
-    		print("BookName  Stock")
-    		for i in list(d.keys()):
-    			print(i,d[i])
-    
-    elif ch==6:
+            print(bn, "Doesn't Exist")
+
+    elif ch == 5:
+        print("\nDisplay Stocks")
+        print("Book Name\tStock")
+        
+        for i in list(d.keys()):
+            print(i, "\t", d[i])
+
+    elif ch == 6:
         break
