b4cfe315b60faca524afd5970b6d6f105776e7ba
rolansy <ronalshoeygeorge@gmail.com>
2024-04-03 14:17:00 +0530

    Refactor plt_graph.py to use numpy and improve plotting functions

--- plt_graph.py
+++ plt_graph.py
@@ -1,39 +1,40 @@
 import matplotlib.pyplot as plt
 import math as m
+import numpy as np
 
 def f1():
-    x = range(-10, 11)
+    x=np.linspace(-10,10,500)
     y = [i for i in x] 
     plt.plot(x,y)
     plt.show()
     
 def f2():
-		x=range(-10,11)
+		x=np.linspace(-10,10,500)
 		y=[i**2 for i in x]
 		plt.plot(x,y)
 		plt.show()
 
 def f3():
-    x=range(-10,11)
+    x=np.linspace(-10,10,500)
     y=[2**i for i in x]
     plt.plot(x,y)
     plt.show()
 
 
 def f4():
-    x=range(-10,11)
+    x=np.linspace(-10,10,500)
     y=[m.sin(i) for i in x]
     plt.plot(x,y)
     plt.show()
     
 def f5():
-    x=range(-10,11)
+    x=np.linspace(-10,10,500)
     y=[m.cos(i) for i in x]
     plt.plot(x,y)
     plt.show()
     
 def f6():
-    x=range(-10,11)
+    x=np.linspace(-10,10,500)
     y=[m.exp(i) for i in x]
     plt.plot(x,y)
     plt.show()
@@ -43,6 +44,8 @@ ch=0
 while (ch!=10):
     plt.xlabel('x-axis')
     plt.ylabel('y-axis')
+    plt.legend()
+    plt.grid(True)
     print("Choose Function to Plot : ")
     print("\n1.y=x\n2.y=x^2\n3.y=2^x\n4.y=sin\n5.y=cosx\n6.y=e^x\n10.Exit\n")
     ch=int(input("Enter Choice : "))
