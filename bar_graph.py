a00958067b2e717298749a3cb4dafff13ed1672c
rolansy <ronalshoeygeorge@gmail.com>
2024-04-03 13:40:53 +0530

    Add bar graph code to display the area of continents

---
+++ bar_graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+countries=['Africe','North America','Asia','Oceana','Europa','South America','Soviet Union']
+areas=[11.7,9.4,10.4,3.3,1.9,6.9,7.9]
+
+plt.bar(countries, areas)
+plt.xlabel('Countries')
+plt.ylabel('Area (millions of square kilometers)')
+plt.title('Area of Continents')
+plt.show()
