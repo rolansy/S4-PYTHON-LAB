51d88d7c2b4b9b7a13e31534ae2f539913a84a2c
rolansy <ronalshoeygeorge@gmail.com>
2024-04-17 12:20:52 +0530

    Add code to calculate t-value and critical value for hypothesis testing in trucks_manual.py

---
+++ trucks_manual.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.stats import t
+
+n=16
+mean_per_mpg=22
+stand_dev=3
+prev_mpg=20
+p=0.05
+
+tobs=(mean_per_mpg-prev_mpg)/(stand_dev/np.sqrt(n))
+print("Calculated T value : {:.3f}".format(tobs))
+
+df=n-1
+
+t_crit=t.ppf(1-p,df)
+
+print("Table Value: {:.3f}".format(t_crit))
+
+if tobs > t_crit:
+    print("Reject Null Hypothesis.")
+else:
+    print("Accept Null Hypothesis.")
+
