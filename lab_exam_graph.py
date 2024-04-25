ec45c1b4644317f2650e855734f58cc0edb541b9
rolansy <ronalshoeygeorge@gmail.com>
2024-04-25 22:02:56 +0530

    graph to gen sin 6 variations

---
+++ lab_exam_graph.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 16:56:31 2024
+
+@author: ronal
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+plt.yticks(np.arange(-1,11,0.5))
+plt.grid(True)
+
+colors=['b','yellow','cyan','magenta','g','r']
+
+x=np.linspace(-10,10,500)
+
+for i in range(6):
+    y=[m.sin(z) for z in x]
+    
+    mx=[j+((i+1)*m.pi)/3 for j in x]
+    plt.plot(mx,y,color=colors[i])
+plt.xlim(0,10)
+plt.show()
